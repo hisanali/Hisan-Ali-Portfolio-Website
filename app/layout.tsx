6ef0b15b6d48f9790df1711a58ac6aca3bf5e976
Hisan <hisanali73@gmail.com>
2026-03-03 18:48:13 +0400

    Revert "Add Google Ads tag AW-17492305502 sitewide"
    This reverts commit 897f52591bd8105af478d0cd91271e82516ba702.

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,17 +21,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PNT5FCX5');`}
         </Script>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-DDNBW2YBFL"
-          strategy="afterInteractive"
-        />
-        <Script id="google-tag-config" strategy="afterInteractive">
-          {`window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-DDNBW2YBFL');
-gtag('config', 'AW-17492305502');`}
-        </Script>
       </head>
       <body>
         <noscript>
